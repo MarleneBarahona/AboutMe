179b51138e48c2c2b6849fd4d092d141a4f66aa7
MarleneBarahona <00118616@uca.edu.sv>
2018-03-24 01:41:07 -0600

    Primer commit

---
+++ app/src/main/java/com/barahona/aboutme/MainActivity.java
@@ -0,0 +1,19 @@
+package com.barahona.aboutme;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
